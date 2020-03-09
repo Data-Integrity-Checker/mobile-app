66ff9a412a0e753118b4e0adb43d28cd77df8864
Conner Gieg <giegconner@gmail.com>
2020-03-09 13:10:29 -0700

    Added warnings tab, performance tab, and settings tab

--- App.js
+++ App.js
@@ -1,78 +1,12 @@
-import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Header, Content, List, ListItem, Text, Left, Right, Icon } from 'native-base';
+import React, { Component } from "react";
+import TabScreen from "./screens/TabScreen";
 
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//     </View>
-//   );
-// }
+export default class FooterTabs extends Component {
 
-export default class ListExample extends Component {
   render() {
     return (
-      <Container>
-        <Header>
-          <Text>Device Attributes</Text>
-        </Header>
-        <Content>
-          <List>
-            <ListItem>
-              <Left>
-                <Text>
-                  Device ID: 1234 {"\n"}
-                  Battery: 70% {"\n"}
-                  Duplicate Data: 433 {"\n"}
-                  Missing Data: 343 {"\n"}
-                  GPS Error: 193
-                </Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-            <ListItem>
-              <Left>
-                <Text>
-                  Device ID: 1235 {"\n"}
-                  Battery: 70% {"\n"}
-                  Duplicate Data: 433 {"\n"}
-                  Missing Data: 343 {"\n"}
-                  GPS Error: 193
-                </Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-            <ListItem>
-              <Left>
-                <Text>
-                  Device ID: 1236 {"\n"}
-                  Battery: 70% {"\n"}
-                  Duplicate Data: 433 {"\n"}
-                  Missing Data: 343 {"\n"}
-                  GPS Error: 193
-                </Text>
-              </Left>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
-          </List>
-        </Content>
-      </Container>
+      <TabScreen/>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
